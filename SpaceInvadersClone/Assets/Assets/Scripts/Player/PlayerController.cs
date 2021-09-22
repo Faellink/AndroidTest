9be136d4aeeb4474ebe4d219f7a219054ed5fc04
Rafael Araujo <rafael0910i@gmail.com>
2021-09-22 15:27:47 -0300

    unity project

---
+++ SpaceInvadersClone/Assets/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    public float fireRate;
+    public float timeToShoot;
+
+    public Transform shooter;
+    public GameObject bulletPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        PlayerShoot();
+    }
+
+    void PlayerMovement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(moveX, moveY, 0f).normalized;
+        
+        //Debug.Log(move);
+
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
+
+    void PlayerShoot()
+    {
+        
+        
+        
+        if (Input.GetKey(KeyCode.Space) && Time.time > timeToShoot)
+        {
+            timeToShoot = Time.time + fireRate;
+            Instantiate(bulletPrefab, shooter.position, Quaternion.identity);
+        }
+    }
+}
